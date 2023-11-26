773763194428410970c47d031bf3d28bfd4b5d66
JAndresNav <149545903+JAndresNav@users.noreply.github.com>
2023-11-26 14:57:21 -0600

    Add files via upload

--- Snake ripes.c.c
+++ Snake ripes.c.c
@@ -7,10 +7,50 @@
 #define SW3 (0x08)
 volatile unsigned int * led_base = (volatile unsigned int *) LED_MATRIX_0_BASE;
 volatile unsigned int * manzana  = (volatile unsigned int *) LED_MATRIX_0_BASE;
+volatile unsigned int * borde  = (volatile unsigned int *) LED_MATRIX_0_BASE;
 volatile unsigned int * d_pad_up = (volatile unsigned int *) D_PAD_0_UP;
 volatile unsigned int * d_pad_do = (volatile unsigned int *) D_PAD_0_DOWN;
 volatile unsigned int * d_pad_le = (volatile unsigned int *) D_PAD_0_LEFT;
 volatile unsigned int * d_pad_ri = (volatile unsigned int *) D_PAD_0_RIGHT;
+
+void PrintFrame(int a,int x,int y,int orientacion,int length){
+    
+
+    if(a == 1){
+    borde += x;
+    borde += y * 35;
+    }
+    
+    for(int i = 0;i < length; i++){
+    *borde = 0xFA8072;
+    if(orientacion == 1){ // arriba es -=
+        
+        borde -= LED_MATRIX_0_WIDTH;
+        
+    }else if(orientacion == 2){ // abajo es +=
+        
+        borde += LED_MATRIX_0_WIDTH;
+        
+    }else if(orientacion == 3){ // izquierda es -=1
+        
+        borde -= 1;
+        
+    }else{ // derecha es +=1
+        
+        borde += 1; 
+    
+    }
+
+        
+    }
+
+    
+    
+    
+
+
+    
+}
 
 void main()
 {
@@ -19,8 +59,20 @@ unsigned int mask = 0;
 int score = 0;
 int i = 0, a, f = 0;
 
+PrintFrame(1,0,0,4,35);
+PrintFrame(0,0,0,2,25);
+//PrintFrame(35,25,1,25);
+//PrintFrame(35,25,3,25);
+
+
+led_base += 0;
+
 led_base += 2;
 led_base += 3 * 35;
+
+manzana += 7;
+manzana += 7 * 15;
+*manzana = 0x008000;
 
 while(1)
 {
@@ -58,6 +110,8 @@ while(1)
     }
     
     if((*led_base == 0xFFD700) && (f == 1))
+        break;
+    else if((*led_base == 0xFA8072) && (f == 1))
         break;
     else
        *led_base = 0xFFD700; 
