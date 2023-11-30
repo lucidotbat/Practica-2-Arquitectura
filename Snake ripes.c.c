e4f3c74404a096daa40c6d3fdab98827939ee8c1
Luis Fernando Ramírez Ramos <98113987+lucidotbat@users.noreply.github.com>
2023-11-29 22:57:30 -0600

    Update Snake ripes.c

--- Snake ripes.c.c
+++ Snake ripes.c.c
@@ -20,132 +20,141 @@ volatile unsigned int * led_cola = (volatile unsigned int *) LED_MATRIX_0_BASE;
 void main()
 {
     
-int snake[LED_MATRIX_0_WIDTH * LED_MATRIX_0_HEIGHT] = {0};
-int frente = 4;
-int cola = 0;
+    int snake[LED_MATRIX_0_WIDTH * LED_MATRIX_0_HEIGHT] = {0};
+    int frente = 4;
+    int cola = 0;
     
-unsigned int mask = 0;
+    unsigned int mask = 0;
 
-int score = 0;
-int i = 0, a, f = 0;
+    int score = 0;
+    int i = 0, a, f = 0;
 
-for(int i = 0; i < LED_MATRIX_0_WIDTH; i++){
-    *bordeLados = 0xFA8072;
-    bordeLados += 35 * 24;
-    *bordeLados = 0xFA8072;
-    bordeLados -= 35 * 24;
-    bordeLados++;
-    }
-    
-    
-for(int h = 0; h < LED_MATRIX_0_HEIGHT; h++){
-    *bordeArriba = 0xFA8072;
-    bordeArriba += 34;
-    *bordeArriba = 0xFA8072;
-    bordeArriba -= 34;
-    bordeArriba += 35;
-    }
-
-
-led_base += 2;
-led_base += 3 * 35;
-led_cola += 2;
-led_cola += 3 * 35;
-
-manzana += 7;
-manzana += 2 * 35;
-*manzana = 0x008000;
-
-int contador = 0;
-
-int movimiento1 = 1, movimiento2 =1;
-while(1)
-{
-    for(int j = 0; j < 1000; j++)
-        a = j;
-        
-    if(manzana == led_base)
+    for(int i = 0; i < LED_MATRIX_0_WIDTH; i++)
     {
-        if(manzana >= 805){
-            contador = 37;
-        }
-        if (movimiento1 == 1){
-            led_base += 1;
-            snake[frente] = 1;
+        *bordeLados = 0xFA8072;
+        bordeLados += 35 * 24;
+        *bordeLados = 0xFA8072;
+        bordeLados -= 35 * 24;
+        bordeLados++;
+    }
+    
+    
+    for(int h = 0; h < LED_MATRIX_0_HEIGHT; h++)
+    {
+        *bordeArriba = 0xFA8072;
+        bordeArriba += 34;
+        *bordeArriba = 0xFA8072;
+        bordeArriba -= 34;
+        bordeArriba += 35;
+    }
+
+
+    led_base += 2;
+    led_base += 3 * 35;
+    led_cola += 2;
+    led_cola += 3 * 35;
+
+    manzana += 7;
+    manzana += 2 * 35;
+    *manzana = 0x008000;
+
+    int contador = 0;
+
+    int movimiento1 = 1, movimiento2 =1;
+    while(1)
+    {
+        for(int j = 0; j < 1000; j++)
+            a = j;
+        
+        if(manzana == led_base)
+        {
+            if(manzana >= 805)
+                contador = 37;
+            if (movimiento1 == 1)
+            {
+                led_base += 1;
+                snake[frente] = 1;
             }
-        else if (movimiento1 == -1){
-            led_base -= 1;
-            snake[frente] = -1;
-        }
-        else if (movimiento1 == -LED_MATRIX_0_WIDTH){
-            led_base -= LED_MATRIX_0_WIDTH;
-            snake[frente ] = -35;
-        }
-        else {
-            led_base += LED_MATRIX_0_WIDTH;
-            snake[frente] = 35;
-        }
-        frente++;
-        *led_base = 0xFFD700;
-        score ++;
-        int mamanzana = 0;
-        int contador = 0;
+            else if (movimiento1 == -1)
+            {
+                led_base -= 1;
+                snake[frente] = -1;
+            }
+            else if (movimiento1 == -LED_MATRIX_0_WIDTH)
+            {
+                led_base -= LED_MATRIX_0_WIDTH;
+                snake[frente ] = -35;
+            }
+            else 
+            {
+                led_base += LED_MATRIX_0_WIDTH;
+                snake[frente] = 35;
+            }
+            frente++;
+            *led_base = 0xFFD700;
+            score ++;
+            int mamanzana = 0;
+            int contador = 0;
         
-        while (mamanzana != 1) {
-            manzana += contador;
-            if (*manzana != 0xFFD700 && *manzana != 0xFA8072) {
-                *manzana = 0x008000;
-                mamanzana = 1;
-            } else {
-                contador++;
+            while (mamanzana != 1) 
+            {
+                manzana += contador;
+                if (*manzana != 0xFFD700 && *manzana != 0xFA8072)
+                {
+                    *manzana = 0x008000;
+                    mamanzana = 1;
+                } 
+                else 
+                    contador++;
+            }
         }
-}
-    }
-    if(*d_pad_up == 1)
-    {
-        movimiento1 = -LED_MATRIX_0_WIDTH;
-        movimiento2 = -35;
-    }
-    if(*d_pad_do == 1)
-    {
-        movimiento1= LED_MATRIX_0_WIDTH;
-        movimiento2 = 35;
-    }
-    if(*d_pad_le == 1)
-    {
-        movimiento1= -1;
-        movimiento2 = -1;
-    }
-    if(*d_pad_ri == 1)
-    {
-        movimiento1 = 1;
-        movimiento2 = 1;
-    }
+        if(*d_pad_up == 1)
+        {
+            movimiento1 = -LED_MATRIX_0_WIDTH;
+            movimiento2 = -35;
+        }
+        if(*d_pad_do == 1)
+        {
+            movimiento1= LED_MATRIX_0_WIDTH;
+            movimiento2 = 35;
+        }
+        if(*d_pad_le == 1)
+        {
+            movimiento1= -1;
+            movimiento2 = -1;
+        }
+        if(*d_pad_ri == 1)
+        {
+            movimiento1 = 1;
+            movimiento2 = 1;
+        }
     
-    led_base += movimiento1;
-    snake[frente] = movimiento2;
-    frente++;
-    led_cola += snake[cola];
-    cola++;
-    f++;
+        led_base += movimiento1;
+        snake[frente] = movimiento2;
+        frente++;
+        led_cola += snake[cola];
+        cola++;
+        f++;
     
-    for (int w = 0; w < 1000; w++){
+        for (int w = 0; w < 1000; w++)
         w+w;
-    }
 
     
-    if((*led_base == 0xFFD700) && (f == 1))
-        break;
-    else if((*led_base == 0xFA8072) && (f == 1))
-        break;
-    else{
-        *led_cola = 0x000000;
-        *led_base = 0xFFD700; }
+        if((*led_base == 0xFFD700) && (f == 1))
+            break;
+        else if((*led_base == 0xFA8072) && (f == 1))
+            break;
+        else
+        {
+            *led_cola = 0x000000;
+            *led_base = 0xFFD700; 
+        }
   
-     f = 0;
+        f = 0;
 
-}
+    }
 
-printf("Tu puntuaci?n es %i", score);
+    printf("PUNTOS: %i", score);
+    return 0;
 
 }
